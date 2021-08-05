9483a86263c901bb8e7798909bfe061e30d989ac
gm2202988 <kmk@gabia.com>
2021-08-05 10:47:47 +0900

    chore: fix log info string

--- src/config/middlewares/logger-class.middleware.ts
+++ src/config/middlewares/logger-class.middleware.ts
@@ -4,7 +4,7 @@ import { Request, Response, NextFunction } from 'express';
 @Injectable()
 export class loggerClassMiddleware implements NestMiddleware {
 	use(req: Request, res: Response, next: NextFunction) {
-		console.log('[loggerFunctionMiddleware] Request url : ', req.originalUrl);
+		console.log('[loggerClassMiddleware] Request url : ', req.originalUrl);
 		next();
 	}
 }
